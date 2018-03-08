c2a25e6e3022003b88c7a27e5e9ad2f68d25aaee
Dimitriy <dleksanov@stuy.edu>
2018-03-08 13:59:48 -0500

    modified

--- svg.js
+++ svg.js
@@ -3,14 +3,31 @@ var svg = document.getElementById("pic");
 var clearButt = document.getElementById("clear");
 var circleR = 0;
 
+var createObj = function(x, y, r, c){
+    var obj = {
+	x:x,
+	y:y,
+	r:r,
+	c:c,
+	el:document.createElementNS("http://www.w3.org/2000/svg", "circle"),
+	display: function(){
+	    this.el.setAttribute("cx", this.x);
+	    this.el.setAttribute("cy", this.y);
+	    this.el.setAttribute("r", this.r);
+	    this.el.setAttribute("fill", this.c);
+	    this.el.addEventListener("click", changeColor);
+	    svg.appendChild(this.el);
+	},
+	remove: function(){
+	    svg.removeChild(this.el);
+	}
+    };
+    return obj;
+}
+
 var placeCircle = function(e){
-    c = document.createElementNS("http://www.w3.org/2000/svg", "circle");
-    c.setAttribute("cx", e.offsetX);
-    c.setAttribute("cy", e.offsetY);
-    c.setAttribute("r", 15);
-    c.setAttribute("fill", "goldenrod");
-    c.addEventListener("click", changeColor, true);
-    svg.appendChild(c);
+    var c = createObj(e.offsetX, e.offsetY, 15, "goldenrod");
+    c.display();
 }
 
 var changeColor = function(e){
